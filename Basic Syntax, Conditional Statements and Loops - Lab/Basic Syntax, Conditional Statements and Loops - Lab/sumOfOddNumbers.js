3c1caea5419f28dfc3f9eadc8c5b6c0c8b792a75
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-01-13 15:59:51 +0200

    solved exercises assignment

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/Basic Syntax, Conditional Statements and Loops - Lab/sumOfOddNumbers.js
@@ -0,0 +1,18 @@
+function solve(n) {
+
+    let sum = 0;
+    let counter = 0;
+
+    for (let i = 1; i <= 100; i += 2) {
+
+        console.log(i);
+        sum += i;
+        counter++;
+        if (counter === n) {
+            console.log(`Sum: ${sum}`);
+            break;
+        }
+    }
+}
+
+solve(3);
