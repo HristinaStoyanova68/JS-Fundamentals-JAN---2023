52d7b308744ec492a4dee080263a55f51f130e27
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-01-19 16:02:46 +0200

    solved exercises assignment

---
+++ Data Types and Variables - Lab/demoAmazingNumbers.js
@@ -0,0 +1,11 @@
+function demoAmazingNumbers(num) {
+    let sum = 0;
+    num = num.toString();
+    for (let i = 0; i < num.length; i++) {
+        sum += Number(num[i]);
+    }
+    let result = sum.toString().includes(`9`);
+    console.log(result ? `${num} Amazing? ${`True`}` : `${num} Amazing? ${`False`}`);
+
+}
+demoAmazingNumbers(1233);
