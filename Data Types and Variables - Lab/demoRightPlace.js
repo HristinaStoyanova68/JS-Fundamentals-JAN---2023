4bf11964f4d9ebcb293614d8d4817c5f01bb1b24
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-01-19 12:20:05 +0200

    solved exercises assignment

--- Data Types and Variables - Lab/demoRightPlace.js
+++ Data Types and Variables - Lab/demoRightPlace.js
@@ -1,1 +1,17 @@
-function demoRightPlace()
+function demoRightPlace(text, charToChange, targetText) {
+    let newText = "";
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] === "_") {
+            newText += charToChange;
+
+        } else {
+            newText += text[i];
+        }
+    }
+    if (newText === targetText) {
+        console.log(`Matched`);
+    } else {
+        console.log(`Not Matched`);
+    }
+}
+demoRightPlace('Str_ng', 'o', 'Strong');
