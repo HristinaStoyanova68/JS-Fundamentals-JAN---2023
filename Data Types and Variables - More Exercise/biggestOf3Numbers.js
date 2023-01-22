1d22fec3166e23b8b717f08c51e2e3cbcef6caea
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-01-22 18:31:43 +0200

    solved exercises assignment

---
+++ Data Types and Variables - More Exercise/biggestOf3Numbers.js
@@ -0,0 +1,22 @@
+function biggestOf3Numbers(num1, num2, num3) {
+
+    let biggestNum = Number.MIN_SAFE_INTEGER;
+    if (num1 > biggestNum) {
+        biggestNum = num1;
+    }
+    if (num2 > num1) {
+        biggestNum = num2;
+    }
+    if (num3 > biggestNum) {
+        biggestNum = num3;
+    }
+
+    console.log(biggestNum);
+
+}
+
+biggestOf3Numbers(2,
+
+    2,
+
+    2);
