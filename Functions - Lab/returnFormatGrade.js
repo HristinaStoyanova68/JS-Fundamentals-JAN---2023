8f5107c8e435ba5a4a0b91e78f675b2c3ba4bdc3
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-02-02 13:14:31 +0200

    solved exercises assignment

---
+++ Functions - Lab/returnFormatGrade.js
@@ -0,0 +1,19 @@
+function returnFormatGrade2(grade) {
+    let assessment = '';
+
+    if (grade >= 2 && grade < 3) {
+        assessment = 'Fail';
+    } else if (grade < 3.50) {
+        assessment = 'Poor';
+    } else if (grade < 4.50) {
+        assessment = 'Good';
+    } else if (grade < 5.50) {
+        assessment = 'Very good';
+    } else if (grade >= 5.50 && grade <= 6.00) {
+        assessment = 'Excellent';
+    }
+    let gradeValueText = grade < 3 ? '2' : grade.toFixed(2);
+    return `${assessment} (${gradeValueText})`;
+}
+
+console.log(returnFormatGrade2(6.00));
