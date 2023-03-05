cfe6de0bd5fb6f23e9f062c68eae5889f7cd9311
HRISTINA STOYANOVA <viara68@abv.bg>
2023-03-05 15:07:40 +0200

    solved exercises assignment

---
+++ Associative Arrays - Exercise/aMinerTask.js
@@ -0,0 +1,40 @@
+function aMinerTask(input) {
+
+    let resourceData = {};
+
+    for (let i = 0; i < input.length; i += 2) {
+        let resource = input[i];
+        let quantity = Number(input[i + 1]);
+        if (!resourceData.hasOwnProperty(resource)) {
+            resourceData[resource] = quantity;
+        } else {
+            resourceData[resource] = resourceData[resource] + quantity;
+        }
+    }
+    let resourceDataAsArr = Object.entries(resourceData);
+
+    for (const [resource, quantity] of resourceDataAsArr) {
+        console.log(`${resource} -> ${quantity}`);
+    }
+}
+
+aMinerTask([
+    'Gold',
+    '155',
+    'Silver',
+    '10',
+    'Copper',
+    '17'
+]);
+console.log('..............');
+aMinerTask([
+    'gold', '155',
+    'silver', '10',
+    'copper', '17',
+    'gold', '15']);
+
+
+
+
+
+
